81d71bad30175c21eee0a7506bfd0f1f2624c8f5
John paul <2401101011@student.buksu.edu.ph>
2025-04-09 09:43:57 +0800

    Reorder quiz menu options for better clarity

--- src/main.c
+++ src/main.c
@@ -10,8 +10,8 @@ int main() {
 
         printf("\033[1;34mWelcome to the QuizWhiz System\033[0m\n\n");
         printf("[1] Make a Quiz\n");
-        printf("[2] View and Take Quizes\n");
-        printf("[3] View student's data\n");
+        printf("[2] View student's data\n");
+        printf("[3] View and Take Quizes\n");
         printf("[4] Exit the system\n\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
@@ -21,10 +21,10 @@ int main() {
             case 1:
                 make_quiz_menu();
                 break;
-            case 2:
+            case 3:
                 view_take_quizzes();
                 break;
-            case 3:
+            case 2:
                 view_student_data();
                 break;
             case 4:
